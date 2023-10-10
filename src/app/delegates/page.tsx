398bd3aabe5bff9a274f715316d3b10e25d2c222
Choover <choover11@gmail.com>
2023-10-10 14:18:47 -0700

    Initial

---
+++ src/app/delegates/page.tsx
@@ -0,0 +1,19 @@
+import directus from '../../lib/directus';
+import { readItems } from '@directus/sdk/rest';
+
+async function getDelegates() {
+	return directus.request(readItems('Delegates'));
+}
+  
+export default async function Delegates() {
+	const delegates = await getDelegates();
+  
+  return (
+    <main>
+      <h2>Delegates</h2>
+      {delegates.map((d) => (
+        <p key={d.id}>{d.name}</p>
+      ))}
+    </main>
+  )
+}
