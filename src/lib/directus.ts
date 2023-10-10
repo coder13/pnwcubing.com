dd85fc2e1160f4c791fa257d5e48bbbd080a7e47
Cailyn Hoover <cailyn.hoover@circle.com>
2023-10-10 14:18:47 -0700

    Initial

---
+++ src/lib/directus.ts
@@ -0,0 +1,19 @@
+import { createDirectus } from '@directus/sdk';
+import { rest } from '@directus/sdk/rest';
+
+interface Delegate {
+  id: string
+  name: string
+  email: string
+  region: string
+  description: string
+  Region: string
+}
+
+interface Schema {
+  Delegates: Delegate[]
+}
+
+const directus = createDirectus<Schema>('https://admin.pnwcubing.com').with(rest());
+
+export default directus;
