81acc7e5c3c8b9a23f3bff1b0c96675ae48c70ad
choover <choover11@gmail.com>
2023-10-11 07:14:50 -0700

    Enabled soft navigations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {children}
+
+        <main className="flex justify-center">
+          <div className="flex flex-col w-2/3">{children}</div>
+        </main>
       </body>
     </html>
   );
