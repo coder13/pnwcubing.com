67b48b9a2a838ddb3f82439ce5e696bfc2492026
choover <choover11@gmail.com>
2023-10-12 07:02:09 -0700

    Cached images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     domains: ["avatars.worldcubeassociation.org"],
+    minimumCacheTTL: 600,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.worldcubeassociation.org",
+      },
+    ],
   },
 };
 
