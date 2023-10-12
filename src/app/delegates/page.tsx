63b224dd8485ab81f63eb815a24254d950be34b5
choover <choover11@gmail.com>
2023-10-11 21:06:22 -0700

    styling work

--- src/app/delegates/page.tsx
+++ src/app/delegates/page.tsx
@@ -1,5 +1,5 @@
 import { DelegateDetails } from "@/components/DelegateDetails";
-import directus from "../../lib/directus";
+import directus, { Delegate } from "../../lib/directus";
 import { readItems } from "@directus/sdk/rest";
 import { Fragment } from "react";
 
@@ -9,16 +9,30 @@ async function getDelegates() {
 
 export default async function Delegates() {
   const delegates = await getDelegates();
+
+  const delegatesGroupedByRegion = delegates.reduce(
+    (acc, d) => ({
+      ...acc,
+      [d.Region]: [...(acc[d.Region] || []), d],
+    }),
+    {} as Record<string, Delegate[]>,
+  );
+
   return (
     <>
       <h2 className="text-4xl">Delegates</h2>
       <br />
       <div className="space-y-4">
-        {delegates.map((d) => (
-          <Fragment key={d.id}>
-            <hr />
-            <DelegateDetails {...d} />
-          </Fragment>
+        {Object.entries(delegatesGroupedByRegion).map(([region, delegates]) => (
+          <div key={region} className="space-y-4">
+            <h3 className="text-3xl">{region}</h3>
+            {delegates.map((d) => (
+              <Fragment key={d.id}>
+                <hr />
+                <DelegateDetails {...d} />
+              </Fragment>
+            ))}
+          </div>
         ))}
       </div>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#1fb6ff",
+      },
     },
   },
   plugins: [require("flowbite/plugin")],
