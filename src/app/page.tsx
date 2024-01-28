103c81a9e4a4c2616ddbb99674d72c3c15ddce50
Cailyn Hoover <choover11@gmail.com>
2024-01-27 16:21:28 -0800

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <>
-      <h1>Pacific Northwest Cubing!</h1>
-    </>
+    <div
+      style={{
+        background: "url(/images/landing.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        opacity: 0.5,
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      {/* <h1>Pacific Northwest Cubing!</h1> */}
+    </div>
   );
 }
