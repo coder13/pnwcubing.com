a19262aa1d3ebced4d6b9a90073498441d43e8de
Cailyn Hoover <choover11@gmail.com>
2024-04-26 15:28:19 -0700

    Switched to accordion for faq

--- src/app/about/competing/page.tsx
+++ src/app/about/competing/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import FAQ from "./faq";
 
 export default function AboutCompeting() {
   return (
-    <main className="flex flex-col justify-center flex-1 leading-8">
+    <main className="flex flex-col justify-center flex-1 leading-8 pb-12">
       <section className="flex justify-center w-full px-4 space-y-4 py-4 bg-white">
         <div className="flex flex-col md:w-2/3 w-full">
           <h1 className="text-4xl">PNW Competitions</h1>
@@ -146,6 +147,7 @@ export default function AboutCompeting() {
                 alt="Nametag Back"
                 width={240}
                 height={480}
+                className="h-[480px]"
               />
               <div className="p-2">
                 <p>On the back, we see:</p>
@@ -224,102 +226,7 @@ export default function AboutCompeting() {
           <h2 id="FAQ" className="text-2xl font-bold">
             Frequently Asked Questions
           </h2>
-          <p className="font-bold">Q: Do I use my own cube to compete?</p>
-          <p>
-            A: Yes! You are responsible for your own hardware in order to
-            compete. Please refer to section 3 of the WCA Regulations for more
-            information on what puzzles are okay to use in competition. If you
-            have any questions, please reach out!
-          </p>
-          <p className="font-bold">Q: What is the competition like?</p>
-          <p>
-            A: Competitions are a ton of fun! Competitions are a great
-            opportuity to meet new cubers, make new friends, and be able to
-            compete and get official times recognized by the World Cube
-            Association. Competitions can be as social of an event as you would
-            like them to be. The communitiy is extremely welcoming and you can
-            always sit at any table and strike up a conversation with another
-            cuber. They are great places to try new cubes as well, just in case
-            you are looking to get a new main cube! Once you walk into the
-            venue, you will most likely see your nametag at a check-in table. If
-            you are a first-timer, you will be asked to verify your information
-            on your registration (name, DOB, country). Once you grab your
-            nametag, go ahead and find a seat and start cubing! There will be a
-            competitor/judge tutorial, which can be found on the schedule. After
-            that, we will begin with the first event and will be calling events
-            up by group. Your group numbers can be found on the back of your
-            nametag. After you compete in an event, you will most likely also be
-            expected to help judge another group (typically the following
-            group). Competitions are very self-sufficient, so we rely heavily on
-            the assitance from other cubers and parents like you! Your assisgned
-            helping groups should also be listed on the back of your nametag.
-            Once you are done competing in your event, you can go back to your
-            table and continnue to socialize! To see if you made it to the next
-            round of an event, you can check your results.
-          </p>
-          <p className="font-bold">Q: How do I find results?</p>
-          <p>
-            A: For the day of competing, WCA Live will have all of the results
-            manually entered throughout the day. Once on WCA Live, scroll to the
-            competition in question and you&apos;re good to go! After the
-            competition, official results will be posted typically within a few
-            days to a week after the competition happens. Q: How do I get a WCA
-            ID? After official results are posted after the competitions (see
-            question above), you will receive an email with information on your
-            WCA ID.
-          </p>
-          <p className="font-bold">
-            Q: Why am I not on the registration list yet?
-          </p>
-          <p>
-            A: Make sure you pay you registration fee in order to be eligible to
-            be placed on the competitor list. If the competitor limit has been
-            reached by the time your registration is completed, you will be
-            placed onto a waitlist, which is listed in the order of when
-            competitors pay. If you are not advanced from the waitlist before
-            the waittlist closes (date found on Register tab), you will be
-            issued a full refund.
-          </p>
-          <p className="font-bold">
-            Q: What is the typical age of competitors at these events?{" "}
-          </p>
-          <p>
-            A: All ages are welcome, but in general competitors are between 10
-            and 20 years old. Don&apos;t let this stop you though! Everyone can
-            and should cube!
-          </p>
-          <p className="font-bold">Q: Are there age divisions?</p>
-          <p>A: Nope! All cometitors compete in the same groups.</p>
-          <p className="font-bold">
-            Q: Is there anything else I should know before attending my first
-            competition?
-          </p>
-          <p>
-            A: Yes! Attending your first competition can come with a lot of
-            nerves, so try and focus on having fun and meeting new people. If
-            you aren&apos;t as fast as Max Park or Feliks Zemdegs, just remember
-            that all cubers faster than you have been at your speed at some
-            point! I also point people to
-            <a
-              href="https://www.worldcubeassociation.org/persons/2007VALK01"
-              target="_blank"
-            >
-              Mats Valk&apos;s WCA Profile
-            </a>
-            since he started competing whe he averaged well over a minute and
-            now averages 6-7 seconds. People often do slightly worse in
-            competition than at home too, so don&apos;t beat yourself up if you
-            don&apos;t do as well as you had hoped! Once you lock in your first
-            set of results, you&apos;ll have new personal records to break and
-            goals to achieve at every new competition! If you plan on bringing
-            any of your own puzzles, we highly rercommend marking them somehow
-            (sharpie mark under a center cap or something similar) or keeping
-            them in a box/bag. It happens far too often that many cubers bring a
-            bunch of puzzles, put them on a table unattended, and accidentally
-            take someone elses&apos; puzzle, thinking it was theirs. It&apos;s
-            easy to lose track of a single puzzle, especially since so many of
-            them look the same at first glance!
-          </p>
+          <FAQ />
         </div>
       </section>
     </main>
